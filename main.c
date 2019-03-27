1e46ca36564634b2a7e9f732a57b0d9524ae0564
RxnNode <rxnnode@gmail.com>
2019-03-27 18:08:50 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,27 +1,52 @@
 #include <stdio.h>
-FILE *dir;
-FILE *name;
+#include <stdlib.h>
+#include "ConvertoBinary.h"
 
 int main() {
 
-    long a = 32, temp;
-    int Arr[64] = {0};
+
+    long temp, a = 43;
+
+    int Arr[64] = {};
 
     for (int i = 0; i < 64; i++){
-
         temp = a % 2;
-
         if ( temp != 0){
             Arr[i] = 1;
         } else{
             Arr[i] = 0;
         }
         if (a != 0) a = a/2;
+    }
+
+
+    int *s[64];
+
+    printf("Srr address: %p\n", s);
+    printf("Arr address: %p\n", Arr);
+
+    for (int k = 0; k < 64; ++k) {
+        s[k] = &Arr[k];
 
     }
-    for (int j = 63; j >= 0 ; j--) {
+
+    printf("Size of S: %d\n", (int)(sizeof(s)/sizeof(s[0])));
+    printf("Size of Arr: %d\n", (int)(sizeof(Arr)/sizeof(Arr[0])));
+
+    printf("Arr :");
+    for (int j = 0; j < (int)(sizeof(Arr)/sizeof(Arr[0])); ++j) {
+
         printf("%d", Arr[j]);
     }
+
+    printf("\nSrr :");
+
+    for (int j = 0; j < (int)(sizeof(s)/sizeof(s[0])); ++j) {
+
+        printf("%d", *s[j]);
+    }
+
+
 
     return 0;
 }
