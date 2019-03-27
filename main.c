d44105af9f99222f04b12714adfb0ccc3986a7f1
RxnNode <rxnnode@gmail.com>
2019-03-27 15:33:17 +0800

    Update main.c
    Decimal to Binary convertion

--- main.c
+++ main.c
@@ -1,10 +1,27 @@
 #include <stdio.h>
+FILE *dir;
+FILE *name;
 
 int main() {
 
-    int a = 0;
-    printf("%d", a);
+    long a = 32, temp;
+    int Arr[64] = {0};
 
+    for (int i = 0; i < 64; i++){
+
+        temp = a % 2;
+
+        if ( temp != 0){
+            Arr[i] = 1;
+        } else{
+            Arr[i] = 0;
+        }
+        if (a != 0) a = a/2;
+
+    }
+    for (int j = 63; j >= 0 ; j--) {
+        printf("%d", Arr[j]);
+    }
 
     return 0;
 }
