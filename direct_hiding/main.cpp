e9a6a27ec4d2ce80953fd030524aeeb4f386d058
RxnNode <rxnnode@gmail.com>
2019-05-02 13:09:13 +0800

    Update main.cpp

--- direct_hiding/main.cpp
+++ direct_hiding/main.cpp
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <math.h>
 #include "img_convert.h"
 
 
@@ -191,19 +192,67 @@ void makemultpic(int n){
 
     }
 }
+
+double psnr(const char *dimg, const char *srcimg){
+    FILE *dis = nullptr;
+    FILE *src = nullptr;
+    int disp = 0, srcp = 0, MSE = 0;
+    int limit = 0;
+    double PSNR = 0;
+    long int sum = 0;
+
+    dis = fopen(dimg,"rb");     //Read text file
+    if (dis == nullptr) {
+        printf("Can't not image file.\n");
+        return -1;
+    }
+    src = fopen(srcimg,"rb");     //Read text file
+    if (src == nullptr) {
+        printf("Can't not simage file.\n");
+        return -1;
+    }
+
+    while ((disp = (unsigned int)fgetc(dis)) != EOF){
+        while ((srcp = (unsigned int)fgetc(src)) != EOF){
+            sum += (srcp - disp)*(srcp - disp);
+        }
+        if (limit  == 512*50){
+            break;
+        } else{
+            limit++;
+        }
+    }
+    printf("Sum: %ld\n",sum);
+    MSE = (int)(sum/(512*512*4));
+    PSNR = 10*log((255*255/MSE));
+
+    return PSNR;
+}
 int main() {
 
     //onvertoBMP("../images/image.jpg","../images/imgb.bmp");
 
-    enc("../images/img.bmp","../text_source/short");
+    //enc("../images/imgb.bmp","../text_source/secret");
+    /*for (int i = 0; i < 8; ++i) {
+        char filename[32];
+        double p = 0.0;
+        sprintf(filename,"../out/out_%d.bmp",i);
+        p = psnr(filename, "../images/imgb.bmp");
+        printf("%d bits hiding psnr : %8f\n",i+1,p);
+    }*/
+    double p = 0.0;
+    p = psnr("../out/out_0.bmp", "../images/imgb.bmp");
+    printf("1 bits hiding psnr : %8f\n",p);
+    p = psnr("../out/out_4.bmp", "../images/imgb.bmp");
+    printf("5 bits hiding psnr : %8f\n",p);
 
     //makemultpic(20);
 
 
-    printf("%d ", 0b00000011<<0);
+    /*printf("%d ", 0b00000011<<0);
     printf("%d ", 0b00000011<<6);
     printf("%d ", 0b00000011<<4);
-    printf("%d ", 0b00000011<<2);
+    printf("%d ", 0b00000011<<2);*/
 
 
 
