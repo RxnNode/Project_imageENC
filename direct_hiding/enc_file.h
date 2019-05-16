8ea1c93c16d9b652c3ac87ee2eea7e33b76a99b0
RxnNode <rxnnode@gmail.com>
2019-05-16 15:13:54 +0800

    Update enc_file.h

--- direct_hiding/enc_file.h
+++ direct_hiding/enc_file.h
@@ -11,7 +11,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
-int enc(const char *img, const char *enc){
+int enc_file(const char *img, const char *enc){
 
 
     unsigned int offset = 0;
@@ -19,15 +19,19 @@ int enc(const char *img, const char *enc){
     FILE *imgr = nullptr;
     FILE *enct = nullptr;
     FILE *fout = nullptr;
-    FILE *log = nullptr;
-    char logc[64];
-
-    log = fopen("../log.txt","w");
 
     /*
-     *  image
-     */
+     *  Debug log
+     *
+        FILE *log = nullptr;
+        char logc[64];
+        log = fopen("../log.txt","w");
+    */
 
+
+    /*
+     *  source image
+     */
     imgr = fopen(img,"rb+");    //Read image file
     if (imgr == nullptr){
         printf("Failed to read image file.");
@@ -37,22 +41,6 @@ int enc(const char *img, const char *enc){
         fread(&offset, sizeof(int), 1, imgr);
         printf("Offset: %d\n",offset);
     }
-
-    /*
-     *  Output file
-     *
-
-    fout = fopen("../images/enc.bmp","wb+");
-    if (fout == NULL){
-        printf("Can't create output file.\n");
-        return -1;
-    } else{
-        fseek(imgr, 0, SEEK_SET);
-        fread(&header, sizeof(char), offset, imgr);     //Read header
-        fwrite(header, sizeof(unsigned char), offset, fout);    // Write header into Output file.
-        printf("Bmp header writing...\n");
-    }
-    */
 
     /*
      *  Read text file and write output file
@@ -73,20 +61,19 @@ int enc(const char *img, const char *enc){
                                  0b01111111 };  //magic
 
 
+        // check folder
+        struct stat st = {0};
+        if (stat("../out",&st) == -1){
+            mkdir("../out",0777);
+        } else{
+            printf("Out is already there!!\n");
+        }
 
         //1 to 8 bits hiding
         for (int j = 0; j < 8; ++j) {
 
             //fseek(imgr, offset, SEEK_SET);  // reset source image to offset
             fseek(enct, 0, SEEK_SET);   // reset text file to start
-
-            // check folder
-            struct stat st = {0};
-            if (stat("../out",&st) == -1){
-                mkdir("../out",0777);
-            } else{
-                printf("Out is already there!!\n");
-            }
 
             // Create multiple files
             char filename[32];
@@ -101,6 +88,8 @@ int enc(const char *img, const char *enc){
                 fwrite(header, sizeof(unsigned char), offset, fout);    // Write header into Output file.
                 printf("%d Bmp header writing...\n",j);
             }
+
+
             int rbits=0, i = 0;
             bool readtext = true, control = true;
             // start hiding
@@ -110,8 +99,8 @@ int enc(const char *img, const char *enc){
 
                 if (tchar != EOF){
                     for (i = 0; i < 8 ; i++) {
-                        sprintf(logc,"i = %d\n",i);
-                        fwrite(logc, sizeof(char),10,log);
+                        //sprintf(logc,"i = %d\n",i);
+                        //fwrite(logc, sizeof(char),10,log);
 
                         if ((imgchar = (unsigned int)fgetc(imgr)) != EOF){
                             for (int k = 0; k <= j ; ++k) {
@@ -124,8 +113,8 @@ int enc(const char *img, const char *enc){
                                     control = true;
                                 }
                                 imgchar = ((imgchar & mask[k])|((tchar & (1 << i)) >> i));
-                                sprintf(logc,"i*= %d\n",i);
-                                fwrite(logc, sizeof(char),10,log);
+                                //sprintf(logc,"i*= %d\n",i);
+                                //fwrite(logc, sizeof(char),10,log);
 
                             }
 
@@ -142,8 +131,8 @@ int enc(const char *img, const char *enc){
 
 
             }
-            sprintf(logc,"%dbits done.\n",j);
-            fwrite(logc, sizeof(char),20,log);
+            //sprintf(logc,"%dbits done.\n",j);
+            //fwrite(logc, sizeof(char),20,log);
             printf("\n");
             printf("%d bits written.\n",rbits);
             // Add rest of pixels
@@ -157,7 +146,7 @@ int enc(const char *img, const char *enc){
     }
     fclose(imgr);
     fclose(enct);
-    fclose(log);
+    //fclose(log);
     return 0;
 
 }
