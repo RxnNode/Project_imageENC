8afb72055743785e49442e13686824d43c2204ed
RxnNode <rxnnode@gmail.com>
2019-05-16 13:55:10 +0800

    Update main.cpp

--- direct_hiding/main.cpp
+++ direct_hiding/main.cpp
@@ -13,46 +13,7 @@
 
 
 
-long double psnr(const char *dimg, const char *srcimg){
 
-    FILE *dis = nullptr;
-    FILE *src = nullptr;
-    int disp = 0, srcp = 0;
-    int limit = 0;
-    long double PSNR = 0, MSE = 0, sum = 0;
-
-    dis = fopen(dimg,"rb");     //Read text file
-    if (dis == nullptr) {
-        printf("Can't not image file.\n");
-        return -1;
-    }
-    src = fopen(srcimg,"rb");     //Read text file
-    if (src == nullptr) {
-        printf("Can't not simage file.\n");
-        return -1;
-    }
-
-    while ((disp = (unsigned int)fgetc(dis)) != EOF){
-
-        if((srcp = (unsigned int)fgetc(src)) != EOF){
-
-            sum += (srcp - disp)*(srcp - disp);
-        }
-
-        if (limit  == 512*512) break; else limit++;
-    }
-
-
-    printf("Sum: %Lf\n",sum);
-    MSE = (sum/(512*512*4));
-    printf("MSE: %Lf\n",MSE);
-    PSNR = 20*log(((2^32-1)/MSE));
-
-    fclose(dis);
-    fclose(src);
-
-    return PSNR;
-}
 int main() {
 
     convertoBMP("../images/lena.png","../images/imgl.bmp");
